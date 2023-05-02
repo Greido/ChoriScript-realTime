c36f468dd602efdd7a4b84ea7b40f9c44ec0bd91
unknown <juanpablocorzobarrera@gmail.com>
2023-05-02 06:46:40 -0300

    Push realtime bby!

--- script.js
+++ script.js
@@ -55,3 +55,47 @@ dropDown.addEventListener('change',(e)=>{
     }
 
 })
+
+
+
+//Datos de los clientes 
+
+let numCliente = ''
+
+const dataCLiente = (e)=>{
+
+    //Me traigo el numero del cliente
+    
+    //Indico el numero del cliente
+    let numClientito = document.getElementById('numCliente')
+
+    console.log(numClientito)
+    //Divivdo la cadena en partes con split() dentro de los () le digo donde dividir en 2
+
+    let numClientitoParts = numClientito.innerHTML.split(':');
+
+    //Si hago esto puedo obtener la segunda parte que es el uno y guardarlo en otra var y enviarlo a la api
+    console.log(numClientitoParts[1])
+
+    let _id = numClientitoParts[1]
+    if (_id===undefined) {
+        console.log(`Numero que mando a la api ` + _id[1])
+        
+    }
+    
+
+    //Me traigo el nombre del cliente
+
+
+        //P con el nombre del cliente
+    let nombreCliente = document.getElementById('nameCliente');
+
+        //Divido en 2 al cliente desp del ":"
+    let nameCliente = nombreCliente.innerHTML.split(':');
+
+
+
+}
+
+
+dataCLiente()
